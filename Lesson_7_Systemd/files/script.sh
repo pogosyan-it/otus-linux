f151add1751007a2bd3fa2e1812f19c59e396e8e
ERPogosyan <pogosyan.it@gmail.com>
2020-06-24 16:47:17 +0300

    Sestemd_last2

--- Lesson_7_Systemd/files/script.sh
+++ Lesson_7_Systemd/files/script.sh
@@ -37,14 +37,3 @@ systemctl start  spawn-fcgi.service
 systemctl enable spawn-fcgi.service
 systemctl start httpd@httpd-conf1.service
 systemctl start httpd@httpd-conf2.service
-
-
-str=$(cat /etc/sysconfig/serv_mon.conf | grep 'key_phrase' | sed 's|.*\=||' | tr -d '"')
-
-num=$(cat /var/log/messages | grep "$str" | wc -l )
-if [[ $num -eq 0 ]]; then
-   echo "`date`  --> Key Phrase was not found in /var/log/masseges" >> /home/vagrant/report.txt
-else
-   echo "`date`  --> Key Phrase ($str) was found in /var/log/masseges $num times" >> /home/vagrant/report.txt
-fi
-
