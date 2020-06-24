8da080bf268b07ca2067744cbc49a1d7ac93e504
ERPogosyan <pogosyan.it@gmail.com>
2020-06-24 16:01:32 +0300

    SYSTEMD

--- Lesson_7_Systemd/script.sh
+++ Lesson_7_Systemd/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #cd /vagrant
-sudo cp /vagrant/logmon.service /vagrant/logmon.timer /etc/systemd/system/
-sudo cp /vagrant/serv_mon.conf /etc/sysconfig/
+sudo mv /vagrant/logmon.service /etc/systemd/system/
+sudo mv /vagrant/logmon.timer /etc/systemd/system/
+sudo mv /vagrant/serv_mon.conf /etc/sysconfig/
 sudo bash /vagrant/random_log.sh &
 
 systemctl daemon-reload
@@ -10,21 +11,32 @@ systemctl start  logmon.timer
 systemctl enable logmon.service
 systemctl start  logmon.service
 
-sudo cp /vagrant/spawn-fcgi /vagrant/httpd-conf1 /vagrant/httpd-conf2 /etc/sysconfig/
+sudo mv /vagrant/spawn-fcgi /etc/sysconfig/
 
 httpd="/etc/sysconfig/httpd"
 if [ -f $httpd ]; then
-   sudo rm /etc/sysconfig/httpd /etc/httpd/conf/httpd.conf
+   sudo cp /etc/sysconfig/httpd  /etc/sysconfig/httpd-conf1
+   sudo cp /etc/sysconfig/httpd  /etc/sysconfig/httpd-conf2
+   sudo mv /etc/sysconfig/httpd  /etc/sysconfig/httpd.bk
+   cat /vagrant/httpd-conf1 > /etc/sysconfig/httpd-conf1
+   cat /vagrant/httpd-conf2 > /etc/sysconfig/httpd-conf2
+   sudo cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd-inst-1.conf
+   sudo cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd-inst-2.conf
+   sudo mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bk
+   cat /vagrant/httpd-inst-1.conf > /etc/httpd/conf/httpd-inst-1.conf
+   cat /vagrant/httpd-inst-2.conf > /etc/httpd/conf/httpd-inst-2.conf
+   sudo cp /usr/lib/systemd/system/httpd.service /etc/systemd/system/httpd@.service
+   sudo cat /vagrant/httpd@.service > /etc/systemd/system/httpd@.service
 fi
 
 
-sudo cp /vagrant/httpd-inst-1.conf /vagrant/httpd-inst-2.conf /etc/httpd/conf/
-sudo cp /vagrant/spawn-fcgi.service /vagrant/httpd@.service /etc/systemd/system/
+#sudo rm -rf  /vagrant/*.*
 
 systemctl daemon-reload
 systemctl start  spawn-fcgi.service
 systemctl enable spawn-fcgi.service
-systemctl start httpd@httpd-inst-1.service
+systemctl start httpd@httpd-conf1.service
+systemctl start httpd@httpd-conf2.service
 
 
 str=$(cat /etc/sysconfig/serv_mon.conf | grep 'key_phrase' | sed 's|.*\=||' | tr -d '"')
