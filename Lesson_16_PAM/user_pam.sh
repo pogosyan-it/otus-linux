8008a874defbb0f063c73f519fbec5d09e0a3b5f
ERPogosyan <pogosyan.it@gmail.com>
2020-08-01 01:18:20 +0300

    PAM

---
+++ Lesson_16_PAM/user_pam.sh
@@ -0,0 +1,27 @@
+#!/bun/bash
+
+/usr/sbin/groupadd admin
+
+for (( i=1; i<=10; i++ )); do
+/usr/sbin/useradd user$i
+echo "passw"$i"rd" | passwd --stdin user$i
+let "j=$i%3"
+if [[ $j -eq 1 ]]; then 
+   /usr/bin/gpasswd -a user$i admin
+fi
+done
+
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd.service
+
+user_array=($(cat /etc/passwd | grep -e '\/bin\/sh\|\/bin\/bash' | sed -e 's/:.*//g'))
+for user in ${user_array[@]}; do
+  user_alow=$(id $user | grep -v admin )
+   if [[ -n $user_alow && "$user" != "root" ]]; then
+       echo "*;*;$user;!Wd0000-2400" >> /etc/security/time.conf
+   fi
+done
+n=$(cat /etc/pam.d/sshd | grep -n "pam_nologin.so" |  sed -e 's/:.*//g')
+string="account    required     pam_time.so"
+let "j=$n+1"
+sed -i "${j}i\\$string" /etc/pam.d/sshd
