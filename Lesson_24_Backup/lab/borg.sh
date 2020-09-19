3e18993a0abae7cfcc2ce94110dd6648c4a6d727
ERPogosyan <pogosyan.it@gmail.com>
2020-09-19 21:04:46 +0300

    Borg

---
+++ Lesson_24_Backup/lab/borg.sh
@@ -0,0 +1,16 @@
+#/bin/bash 
+
+Host="192.168.11.160"
+backup_dir="/etc"
+backup_repo="/var/backup1/"
+
+borg list root@$Host:$backup_repo
+
+if [[ $? -eq 2 ]]; then
+    BORG_NEW_PASSPHRASE= borg init --encryption=keyfile -v root@$Host:$backup_repo    #Создать репу для бэкапов на удаленном сервере зашифрованную ключем н$
+else
+   echo "Repo created"
+fi
+
+borg create -v --stats root@$Host:$backup_repo::$(hostname)_'{now:%Y-%m-%d-%H-%M}' $backup_dir   &>>/var/log/borg/borg.log
+borg prune -v --list --keep-within=4H --keep-daily=92 --keep-monthly=9 root@$Host:$backup_repo &>>/var/log/borg/borg.log
